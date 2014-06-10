87c03145661cc88f55d518779f7d819986c9476b
Hunter Haugen <hunter@puppetlabs.com>
2014-06-10 16:18:14 -0700

    Reduce template code duplication
    The listen template is essentially just a combination of
    frontend/backends in haproxy, and lots of code was duplicated between
    the three templates. This commit moves the stanzas into fragments.

--- spec/defines/frontend_spec.rb
+++ spec/defines/frontend_spec.rb
@@ -14,7 +14,7 @@ describe 'haproxy::frontend' do
     it { should contain_concat__fragment('croy_frontend_block').with(
       'order'   => '15-croy-00',
       'target'  => '/etc/haproxy/haproxy.cfg',
-      'content' => "\nfrontend croy\n  bind 1.1.1.1:18140\n  option  tcplog\n"
+      'content' => "\nfrontend croy\n  bind 1.1.1.1:18140 \n  option  tcplog\n"
     ) }
   end
   context "when an array of ports is provided" do
@@ -32,7 +32,7 @@ describe 'haproxy::frontend' do
     it { should contain_concat__fragment('apache_frontend_block').with(
       'order'   => '15-apache-00',
       'target'  => '/etc/haproxy/haproxy.cfg',
-      'content' => "\nfrontend apache\n  bind 23.23.23.23:80\n  bind 23.23.23.23:443\n  option  tcplog\n"
+      'content' => "\nfrontend apache\n  bind 23.23.23.23:80 \n  bind 23.23.23.23:443 \n  option  tcplog\n"
     ) }
   end
   context "when a comma-separated list of ports is provided" do
@@ -47,7 +47,7 @@ describe 'haproxy::frontend' do
     it { should contain_concat__fragment('apache_frontend_block').with(
       'order'   => '15-apache-00',
       'target'  => '/etc/haproxy/haproxy.cfg',
-      'content' => "\nfrontend apache\n  bind 23.23.23.23:80\n  bind 23.23.23.23:443\n  option  tcplog\n"
+      'content' => "\nfrontend apache\n  bind 23.23.23.23:80 \n  bind 23.23.23.23:443 \n  option  tcplog\n"
     ) }
   end
 end
