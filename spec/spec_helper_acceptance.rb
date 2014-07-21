97db9816a55190d7ce5d53220023bb009403986d
Hunter Haugen <hunter@puppetlabs.com>
2014-07-21 15:08:01 -0700

    Rearrange helper to run tests standalone
    Also correct package names and selinux on rhel7

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -31,10 +31,40 @@ RSpec.configure do |c|
       on host, puppet('module','install','puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
       if fact('operatingsystem') == 'Debian'
         on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
-        apply_manifest('
+        apply_manifest(%{
           include apt
           include apt::backports
-        ')
+        })
+      end
+      pp = <<-EOS
+        $netcat = $::osfamily ? {
+          'RedHat' => $::operatingsystemmajrelease ? {
+            '7'     => 'nmap-ncat',
+            default => 'nc',
+          },
+          'Debian' => 'netcat-openbsd',
+        }
+        package { $netcat: ensure => present, }
+        package { 'screen': ensure => present, }
+        if $::osfamily == 'RedHat' {
+          service { 'iptables': ensure => stopped, }
+          exec { 'setenforce Permissive':
+            path   => ['/bin','/usr/bin','/sbin','/usr/sbin'],
+            onlyif => 'getenforce | grep Enforcing',
+          }
+          if $::operatingsystemmajrelease == '7' {
+            # For `netstat` for serverspec
+            package { 'net-tools': ensure => present, }
+          }
+        }
+      EOS
+      apply_manifest(pp, :catch_failures => true)
+
+      ['5556','5557'].each do |port|
+        shell(%{echo 'while :; do echo "HTTP/1.1 200 OK\r\n\r\nResponse on #{port}" | nc -l #{port} ; done' > /root/script-#{port}.sh})
+        shell(%{/usr/bin/screen -dmS script-#{port} sh /root/script-#{port}.sh})
+        sleep 1
+        shell(%{netstat -tnl|grep ':#{port}'})
       end
     end
   end
