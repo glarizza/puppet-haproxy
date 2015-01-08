c607a6ae4fb9b24399e270ccf6caa7f545b43dc0
Peter Souter <peter.souter@puppetlabs.com>
2015-01-08 09:36:27 -0800

    (MODULES-1619) Adds spec for when haproxy missing
    Also added new logic around the which parameter for checking haproxy present before running the fact

--- spec/unit/facter/haproxy_version_spec.rb
+++ spec/unit/facter/haproxy_version_spec.rb
@@ -5,12 +5,22 @@ describe Facter::Util::Fact do
     Facter.clear
   }
 
-  it do
-    haproxy_version_output = <<-EOS
-HA-Proxy version 1.5.3 2014/07/25
-Copyright 2000-2014 Willy Tarreau <w@1wt.eu>
-    EOS
-    Facter::Util::Resolution.expects(:exec).with("haproxy -v 2>&1").returns(haproxy_version_output)
-    Facter.fact(:haproxy_version).value.should == "1.5.3"
+  context "haproxy present" do
+    it do
+      haproxy_version_output = <<-EOS
+      HA-Proxy version 1.5.3 2014/07/25
+      Copyright 2000-2014 Willy Tarreau <w@1wt.eu>
+      EOS
+      Facter::Util::Resolution.expects(:which).with("haproxy").returns(true)
+      Facter::Util::Resolution.expects(:exec).with("haproxy -v 2>&1").returns(haproxy_version_output)
+      Facter.fact(:haproxy_version).value.should == "1.5.3"
+    end
+  end
+
+  context "haproxy not present" do
+    it do
+      Facter::Util::Resolution.expects(:which).with("haproxy").returns(false)
+      Facter.fact(:haproxy_version).should be_nil
+    end
   end
 end
