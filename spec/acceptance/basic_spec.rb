07a819e0ea3f88db6d1534cc8094fa2e011b8845
Ciaran McCrisken <ciaran.mccrisken@puppet.com>
2020-09-29 18:19:20 +0100

    Override  $sort_options_alphabetic when 'httpchk' option defined
    When using the option 'httpchk', it must be positioned before the
    'http-check' directive in haproxy.cfg otherwise it will be ignored.
    
    A warning will be output to stderr to inform the user that the
    value of $sort_options_alphabetic is being overridden.
    
    Also added an acceptance to ensure the expected ordering is taking
    place and a warning is presented.
    
    Add is_hash check for $options param in haproxy::backend

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -75,6 +75,7 @@ describe 'configuring haproxy' do
           options   => { 'stats' => ['uri /','auth puppet:puppet'], },
         }
       PUPPETCODE
+
       it 'starts' do
         retry_on_error_matching do
           apply_manifest(pp_three, catch_failures: true)
@@ -86,6 +87,44 @@ describe 'configuring haproxy' do
           run_shell("/usr/bin/curl -u puppet:puppet localhost:#{port}") do |r|
             expect(r.stdout).to contain %r{HAProxy}
             expect(r.exit_code).to eq 0
+          end
+        end
+      end
+
+      context 'when "httpchk" option is defined and $sort_options_aphabetic => true' do
+        pp_httpchk_option = <<-PUPPETCODE
+        class { 'haproxy::globals':
+          sort_options_alphabetic => true,
+        }
+        class { 'haproxy': }
+        haproxy::listen { 'stats':
+          ipaddress => '127.0.0.1',
+          ports     => ['9091'],
+          mode      => 'http',
+        }
+        haproxy::backend { 'servers':
+          mode => 'http',
+          sort_options_alphabetic => true,
+          options => {
+            'option'  => [
+              'httpchk',
+            ],
+            'http-check' => 'disable-on-404',
+            'server' => [
+               'srv1 127.0.0.1:9091 check',
+            ],
+          },
+        }
+        PUPPETCODE
+
+        it 'overrides $sort_options_aphabetic to false and warn' do
+          apply_manifest(pp_httpchk_option, catch_failures: true) do |r|
+            expect(r.stderr).to contain %r{Overriding\sthe\svalue\sof\s\$sort_options_alphabetic\sto\s"false"\sdue\sto\s"httpchk"\soption\sdefined}
+          end
+        end
+        describe file('/etc/haproxy/haproxy.cfg') do
+          its(:content) do
+            is_expected.to match %r{backend\sservers\n\s+mode\shttp\n\s+option\shttpchk\n\s+http-check\s+disable-on-404}
           end
         end
       end
